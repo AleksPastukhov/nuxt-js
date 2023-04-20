6e1b9a5b8abefc0a5998f7b52909c89207e6c189
AleksPastukhov <pastukhov.os@icloud.com>
2023-04-20 15:58:33 +0300

    add project

---
+++ pages/about.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci,
+      consectetur assumenda quisquam qui veritatis numquam dolorem in minima
+      deserunt perferendis.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci,
+      consectetur assumenda quisquam qui veritatis numquam dolorem in minima
+      deserunt perferendis.
+    </p>
+    <div>{{ message.message }}</div>
+  </div>
+</template>
+
+<script setup>
+const { data: message } = await useFetch("/api/ninja?name=Mario", {
+  method: "post",
+  body: { age: "30" },
+});
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
